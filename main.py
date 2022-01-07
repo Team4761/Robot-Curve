1ec10dae57185e9082114673b28f1a9c163586e8
Gian Sabia <43626786+SgnulRuEmmig@users.noreply.github.com>
2022-01-06 21:02:04 -0500

    Day 1 Code
    horrible and primitive python code

---
+++ main.py
@@ -0,0 +1,9 @@
+#import vectors
+
+p = vec2(5,2)
+
+print(p.x)
+print(p.y)
+
+while(1):
+    print("")
---
+++ vectors.py
@@ -0,0 +1,32 @@
+#Remaking all the stuff that'd I'd use for shadertoy and the desmos program
+
+class vec2:
+
+    x = 0
+    y = 0
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+def print(v):
+    print("(" + str(v.x) + "," + str(v.y) + ")")
+    
+def length(v):
+    return math.sqrt(v.x*v.x + v.y*v.y);
+
+def dot(v1, v2):
+    dotx = v1.x * v2.x
+    doty = v1.y * v2.y
+    
+    return dotx + doty
+    
+def normalize(v):
+    l = length(v)
+    
+    return vec2(v.x/l,v.y/l)
+    
+def rotate90(v):
+    return vec2(-v.y,v.x)
+    
+
